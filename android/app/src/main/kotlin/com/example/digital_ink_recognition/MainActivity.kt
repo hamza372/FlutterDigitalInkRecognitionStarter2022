844bbd2cb4860bec012297ec410d2ae04f8a7033
hamza372 <31725222+hamza372@users.noreply.github.com>
2022-11-01 07:47:27 -0700

    2022

---
+++ android/app/src/main/kotlin/com/example/digital_ink_recognition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.digital_ink_recognition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ build/app/generated/source/buildConfig/debug/com/example/digital_ink_recognition/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.digital_ink_recognition;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.digital_ink_recognition";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
